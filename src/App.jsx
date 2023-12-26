731d2e016d0d3c56e5ee37ba96f8a4937ac9b945
IbrahimGazi_2022~ <coder.ibrahimgazi@gmail.com>
2023-12-26 06:45:58 +0600

    Complete project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import MainComp from "./components/MainComp/MainComp";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+      <MainComp />
+    </div>
+  );
+};
+
+export default App;
